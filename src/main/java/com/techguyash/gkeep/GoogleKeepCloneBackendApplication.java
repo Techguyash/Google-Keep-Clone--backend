c26390b84b7ae229711199a486d81e3a58476a3d
techguyash <techguyash@gmail.com>
2023-11-18 13:25:27 +0530

    Cloud : Azure mysql, Keyvault commit

--- src/main/java/com/techguyash/gkeep/GoogleKeepCloneBackendApplication.java
+++ src/main/java/com/techguyash/gkeep/GoogleKeepCloneBackendApplication.java
@@ -1,15 +1,31 @@
 package com.techguyash.gkeep;
 
+import com.azure.security.keyvault.secrets.SecretClient;
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Data
 public class GoogleKeepCloneBackendApplication
 {
+    private final SecretClient secretClient;
+
+    @Value("${user.test}")
+    private String userString;
 
     public static void main(String[] args)
     {
         SpringApplication.run(GoogleKeepCloneBackendApplication.class, args);
+    }
+
+    @PostConstruct
+    public void loadKV()
+    {
+        //String value = secretClient.getSecret("db-password").getValue();
+        System.out.println("DB url retrieved :"+userString);
     }
 
 }
